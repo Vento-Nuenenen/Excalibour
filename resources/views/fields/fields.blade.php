6340144c03b89236e397c4223bd8abe117e38b2e
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-08-27 20:43:16 +0200

    Update fields.blade.php

--- resources/views/fields/fields.blade.php
+++ resources/views/fields/fields.blade.php
@@ -11,14 +11,14 @@
         <div class="card mb-3">
             <div class="card-header">
                 {!! Form::open(array('route' => 'fields', 'method' => 'POST', 'role' => 'form', 'class' => 'needs-validation')) !!}
-                    <div class="input-group" id="adv-search">
-                        {!! Form::text('search', NULL, array('id' => 'search', 'class' => 'form-control', 'placeholder' => 'Suche')) !!}
-                        <div class="input-group-append">
-                            <button type="submit" class="btn btn-primary form-control">
-                                <span class="fa fa-search"></span>
-                            </button>
-                        </div>
+                <div class="input-group" id="adv-search">
+                    {!! Form::text('search', NULL, array('id' => 'search', 'class' => 'form-control', 'placeholder' => 'Suche')) !!}
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-primary form-control">
+                            <span class="fa fa-search"></span>
+                        </button>
                     </div>
+                </div>
                 {!! Form::close() !!}
                 <div class="input-group" id="adv-search">
                     <button onclick="location.href='{{ route('add-fields') }}'" type="button" class="btn btn-primary form-control mt-2">Neuer Posten</button>
@@ -38,37 +38,37 @@
                 <div class="card-body table-responsive">
                     <table class="table table-hover">
                         <thead>
-                            <th>
-                                Name
-                            </th>
-                            <th>
-                                Beschreibung
-                            </th>
-                            <th>
-                                Punkte
-                            </th>
-                            <th>
-                                Optionen
-                            </th>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Beschreibung
+                        </th>
+                        <th>
+                            Punkte
+                        </th>
+                        <th>
+                            Optionen
+                        </th>
                         </thead>
                         <tbody>
-                            @foreach($fields as $field)
-                                <tr>
-                                    <td>
-                                        {{ $field->field_name }}
-                                    </td>
-                                    <td>
-                                        {{ $field->field_description }}
-                                    </td>
-                                    <td>
-                                        {{ $field->MAX_POINTS }}
-                                    </td>
-                                    <td>
-                                        <button onclick="location.href='{{ route('edit-fields',$field->id) }}'" class="btn btn-danger ml-2"><span class="fa fa-edit"></span></button>
-                                        <button onclick="location.href='{{ route('destroy-fields',$field->id) }}'" class="btn btn-danger ml-2"><span class="fa fa-remove"></span></button>
-                                    </td>
-                                </tr>
-                            @endforeach
+                        @foreach($fields as $field)
+                            <tr>
+                                <td>
+                                    {{ $field->field_name }}
+                                </td>
+                                <td>
+                                    {{ $field->field_description }}
+                                </td>
+                                <td>
+                                    {{ $field->MAX_POINTS }}
+                                </td>
+                                <td>
+                                    <button onclick="location.href='{{ route('edit-fields',$field->id) }}'" class="btn btn-danger ml-2"><span class="fa fa-edit"></span></button>
+                                    <button onclick="location.href='{{ route('destroy-fields',$field->id) }}'" class="btn btn-danger ml-2"><span class="fa fa-remove"></span></button>
+                                </td>
+                            </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
